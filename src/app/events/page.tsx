951b2cfcce5adc12e01026724ee0f8c8818d8c18
zNebula <JoshSheerin@outlook.com>
2026-07-08 15:24:03 +0100

    feat: create events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,47 +1,72 @@
 'use client';
 
 import PageLayout from '@/app/genericLayout';
-import { ReactNode } from 'react';
-import { events } from '../data';
-import { Button, Image, List, Stack } from '@mantine/core';
-import styles from './events.module.css';
+import { ReactNode, useState } from 'react';
+import { Event, events } from '../data';
+import { ActionIcon, Box, Button, Flex, Grid, Image, List, SimpleGrid, Stack, Tabs, Title } from '@mantine/core';
+import { EventCard } from '../components/eventCard';
+import { EventModal } from '../components/eventModal';
+import { useDisclosure } from '@mantine/hooks';
+import { SlidersHorizontalIcon } from '@phosphor-icons/react';
 
 export default function Events(): ReactNode {
   const eventsData = events;
+  const dates: string[] = [...new Set<string>(events.map((event) => event.date))];
+  const[selectedEvent,setSelectedEvent] = useState<Event>();
+  const [ opened, {open,close}] = useDisclosure()
+  const clickCard = (event:Event) => {
+    setSelectedEvent(event);
+    open();
+  }
   return (
+    <>
+    
     <PageLayout>
-      <Stack justify='center'>
-        <h2>Events</h2>
-        <List type='unordered'>
-          {eventsData.map((event) => (
-            <List.Item
-              key={event.title}
-              className={styles.event}
-            >
-              <h3>{event.title}</h3>
-              <p>{event.date}</p>
-              <p>{event.description}</p>
-
-              <Button
-                component='a'
-                className={styles.button}
-                href={event.ticketLink}
-              >
-                Tickets
-              </Button>
-              {event.image && (
-                <Image
-                  className={styles.image}
-                  src={event.image}
-                  h={300}
-                  width={'auto'}
-                  fit='contain'
-                />
-              )}
-            </List.Item>
+      <EventModal event={selectedEvent} opened={opened} onClose={close}/>
+      <Stack w="90%" m="auto" justify='center'>
+        <Title c="var(--brown)" order={1}>Map</Title>
+        <Grid>
+          <Grid.Col span={8}>
+            <Box bdrs="md" w="100%" h={800} bg="var(--pink)"></Box> {/* [TODO]: Add Map Functionality... Eventually */}
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <List>
+              <List.Item>Test</List.Item>
+            </List>
+          </Grid.Col>
+        </Grid>
+        <Flex align="center" gap="lg">
+        <Title c="var(--brown)" order={1}>Events</Title>
+        <ActionIcon variant='subtle' color="var(--oliveGreen)">
+          <SlidersHorizontalIcon />
+        </ActionIcon>
+        </Flex>
+        <Tabs defaultValue={dates[0]}>
+          <Tabs.List>
+            {dates.map((date) => (
+              <Tabs.Tab size={50} key={date} value={date}>{date}</Tabs.Tab>
+            ))}
+            
+          </Tabs.List>
+          {dates.map((date) => (
+            <Tabs.Panel key={date} value={date}>
+              <EventsView eventsData={eventsData.filter((event) => event.date == date)} clickCard={clickCard} />
+            </Tabs.Panel>
           ))}
-        </List>
+        </Tabs>
+        
       </Stack>
     </PageLayout>
+    </>
   );
+}
+
+function EventsView({eventsData,clickCard}:{eventsData:Event[],clickCard:(event:Event) => void}){
+  return (<SimpleGrid spacing="md" cols={3}>
+          {eventsData.map((event) => (
+
+              <EventCard onClick={clickCard} event={event} />
+              
+          ))}
+  </SimpleGrid>)
 }
